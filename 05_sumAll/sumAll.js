09d2c9701d676e5de55a9fcd0b1e6e9b41881116
shota <55336324+bet-lb2@users.noreply.github.com>
2025-02-07 14:46:40 +0900

    05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    }
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+    const min = first > second ? second : first;
+    const max = first > second ? first : second;
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
